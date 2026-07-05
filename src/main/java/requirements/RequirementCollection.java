cc0222ee78783724419074e28a6d18f29b30de72
reginaleis <128072671+reginaleis@users.noreply.github.com>
2026-07-05 12:44:15 +0200

    home shows some stats

--- src/main/java/requirements/RequirementCollection.java
+++ src/main/java/requirements/RequirementCollection.java
@@ -12,13 +12,13 @@ public class RequirementCollection
 {
     private final List<Requirement> requirements = new ArrayList<>();
 
-    /**
-     * Creates a new instance of RequirementOverview
-     */
     public RequirementCollection()
     {
         requirements.add(new Requirement(1, "Autor 1", "Inhalt 1", new ArrayList<>(), "Draft", "Functional"));
         requirements.add(new Requirement(2, "Autor 2", "Inhalt 2", new ArrayList<>(), "Approved", "Non-Functional"));
+        requirements.add(new Requirement(3, "Autor 2", "Inhalt 3", new ArrayList<>(), "Draft", "Functional"));
+        requirements.add(new Requirement(4, "Autor 1", "Inhalt 4", new ArrayList<>(), "Approved", "Non-Functional"));
+
     }
 
     public List<Requirement> getRequirements()
