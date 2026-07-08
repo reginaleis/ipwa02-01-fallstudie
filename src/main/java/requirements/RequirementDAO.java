543185e83329ff703b7d9b50ecc5f273705f0331
reginaleis <128072671+reginaleis@users.noreply.github.com>
2026-07-08 21:22:38 +0200

    cleanup DAO again with code example repo

--- src/main/java/requirements/RequirementDAO.java
+++ src/main/java/requirements/RequirementDAO.java
@@ -2,11 +2,9 @@ package requirements;
 
 import java.util.List;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -16,65 +14,50 @@ import jakarta.persistence.criteria.Root;
 @Named
 @ApplicationScoped
 public class RequirementDAO {
-    EntityManagerFactory entityManagerFactory;
 
-    @PostConstruct
-    void init() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("require4testing");
+    EntityManager em;
+
+    CriteriaBuilder cb;
+
+    public RequirementDAO() {
+        em = Persistence.createEntityManagerFactory("require4testing").createEntityManager();
+        cb = em.getCriteriaBuilder();
     }
 
     public List<Requirement> getAll() {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        try {
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Requirement> cq = cb.createQuery(Requirement.class);
-            Root<Requirement> root = cq.from(Requirement.class);
-            cq.select(root).orderBy(cb.asc(root.get("id")));
-            return em.createQuery(cq).getResultList();
-        } finally {
-            em.close();
-    }
+        
+        CriteriaQuery<Requirement> cq = cb.createQuery(Requirement.class);
+        Root<Requirement> root = cq.from(Requirement.class);
+        cq.select(root).orderBy(cb.asc(root.get("id")));
+        return em.createQuery(cq).getResultList();
+        
     }
 
     public long getRequirementsCount() {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        try {
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Long> cq = cb.createQuery(Long.class);
-            cq.select(cb.count(cq.from(Requirement.class)));
-            return em.createQuery(cq).getSingleResult();
-        } finally {
-            em.close();
-    }}
+       
+        CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+        cq.select(cb.count(cq.from(Requirement.class)));
+        return em.createQuery(cq).getSingleResult();
+       }
 
     public Requirement getRequirementAtIndex(int pos) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        try {
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Requirement> cq = cb.createQuery(Requirement.class);
-            Root<Requirement> root = cq.from(Requirement.class);
-            cq.select(root).orderBy(cb.asc(root.get("id")));
-            return em.createQuery(cq)
-                .setFirstResult(pos)
-                .setMaxResults(1)
-                .getSingleResult();
-        } finally {
-            em.close();
-    }}
+        
+        CriteriaQuery<Requirement> cq = cb.createQuery(Requirement.class);
+        Root<Requirement> root = cq.from(Requirement.class);
+        cq.select(root).orderBy(cb.asc(root.get("id")));
+        return em.createQuery(cq)
+            .setFirstResult(pos)
+            .setMaxResults(1)
+            .getSingleResult();
+        }
 
     public EntityTransaction getAndBeginTransaction() {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        try {
-            EntityTransaction tran = em.getTransaction();
-            tran.begin();
-            return tran;
-        } finally {
-            em.close();
-        }
+        EntityTransaction tran = em.getTransaction();
+        tran.begin();
+        return tran;
     }
 
     public void merge(Requirement req) {
-        EntityManager em = entityManagerFactory.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         try {
             tx.begin();
@@ -85,12 +68,11 @@ public class RequirementDAO {
                 tx.rollback();
             }
             throw e;
-        } finally {
-            em.close();
-        }
+        } 
     }
+
+
     public void persist(Requirement req) {
-        EntityManager em = entityManagerFactory.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         try {
             tx.begin();
@@ -101,8 +83,6 @@ public class RequirementDAO {
                 tx.rollback();
             }
             throw e;
-        } finally {
-            em.close();
         }
     }
 
