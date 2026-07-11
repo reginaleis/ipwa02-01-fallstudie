2010c73553fc6a5be68de199bef2edb2bf64da8b
reginaleis <128072671+reginaleis@users.noreply.github.com>
2026-07-11 15:11:32 +0200

    define persistence in testplan

--- src/main/java/testplan/Testplan.java
+++ src/main/java/testplan/Testplan.java
@@ -1,22 +1,38 @@
 package testplan;
-import java.util.List;
-import java.time.LocalDateTime;
+import testcases.Testcase;
+
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
-@Named
-@RequestScoped
 
+@Entity
+@Table(name="Testplans")
 public class Testplan {
     
+    @Id
     private Long id;
     private String user;
     private LocalDateTime startTime;
     private String testbench;
     private Duration estimatedDuration;
     private String result;
+    @OneToMany
+    @JoinTable(
+        name = "Testplans_Testcases",
+        joinColumns = @JoinColumn(name = "testplan_id"),
+        inverseJoinColumns = @JoinColumn(name = "testcase_id"))
+    private Set<Testcase> testcases;
 
     public Testplan(int id, String user, LocalDateTime startTime, String testbench, Duration estimatedDuration, String result)
     {
@@ -26,6 +42,7 @@ public class Testplan {
         this.testbench = testbench;
         this.estimatedDuration = estimatedDuration;
         this.result = result;
+        this.testcases = new HashSet<>();
     }
 
     public Long getId()
@@ -89,5 +106,13 @@ public class Testplan {
         this.result = result;
     }
 
+    public Set<Testcase> getTestcases()
+    {
+        return testcases;
+    }
 
+    public void setTestcases(Set<Testcase> testcases)
+    {
+        this.testcases = testcases;
+    }
 }
