57bca84c8ffd230aa4ab9f0d7fbf971acaeb7e4b
reginaleis <128072671+reginaleis@users.noreply.github.com>
2026-07-27 15:21:59 +0200

    make applicationDAO session scoped

--- src/main/java/app/ApplicationDAO.java
+++ src/main/java/app/ApplicationDAO.java
@@ -2,7 +2,7 @@ package app;
 
 import java.util.List;
 
-import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
@@ -14,7 +14,7 @@ import requirements.Requirement;
 import testcases.Testcase;
 
 @Named
-@ApplicationScoped
+@SessionScoped
 public class ApplicationDAO {
 
     EntityManager em;
